4b8df18841903d92dedb53ef0a2c97bdaa4db857
wonjin hur <hwjj1004@gmail.com>
2020-03-08 20:48:52 +0900

    first push

---
+++ src/main/java/com/snapcollector/webservice/Application.java
@@ -0,0 +1,13 @@
+package com.snapcollector.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
