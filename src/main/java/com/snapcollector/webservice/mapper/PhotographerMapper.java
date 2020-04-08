d9f0eda703f2418806642dc14639a272470f889c
wonjin-hur <hwjj1004@gmail.com>
2020-04-09 00:29:57 +0900

    mapper annotation added

--- src/main/java/com/snapcollector/webservice/mapper/PhotographerMapper.java
+++ src/main/java/com/snapcollector/webservice/mapper/PhotographerMapper.java
@@ -2,10 +2,11 @@ package com.snapcollector.webservice.mapper;
 
 import com.snapcollector.webservice.domain.Photographer;
 import com.snapcollector.webservice.domain.PhotographerScore;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-
+@Mapper
 public interface PhotographerMapper {
 
     Photographer findByName(String name);
